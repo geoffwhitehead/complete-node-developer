645547e12e72393189e0db454c1a649b2b12d3bd
Geoff Whitehead <geoff1012@gmail.com>
2018-02-16 13:15:47 +0000

    now passing address as parameter and printing results

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,8 +1,26 @@
 const request = require('request')
+const yargs = require('yargs');
+
+
+const argv = yargs
+.options( {
+  a: {
+    demand: true, 
+    alias: 'address',
+    describe: 'Addres to fetch',
+    string: true
+  }
+})
+.help()
+.alias('help', 'h')
+.argv;
+
+let encodedAddress = encodeURIComponent(argv.a);
 
 //key AIzaSyDRqstjwYqwzo7-iJD2T_FtU3d_Pk3-3Wo
-const url = 'https://maps.googleapis.com/maps/api/geocode/json?address=generator%20studios%20trafalgar%20street%20newcastle%20upon%20tyne'
+const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
 const key = 'AIzaSyDRqstjwYqwzo7-iJD2T_FtU3d_Pk3-3Wo'
+
 request({
   url: url+'&key='+key,
   json: true,
